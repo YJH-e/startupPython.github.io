b718ae59ca20605f467c84d8f87857fad3ecbb57
YJH-e <85558095+YJH-e@users.noreply.github.com>
2021-09-12 15:14:41 +0100

    Update 04.division.py

--- 04.division.py
+++ 04.division.py
@@ -5,15 +5,11 @@ divident = int(input("Enter a whole integer"))
 divisor = float(input("Enter a divisor"))
 
 #whole number division
-#displaying no decimals
+#displaying no decimals using integer function
 print(int(divident // divisor))
 
 #reminder number division
-<<<<<<< HEAD
-#displaying no decimals with another method
+#displaying no decimals using round function
 print(round(divident % divisor))
-=======
-print(divident % divisor) 
 
 ## GACS - ood. Works well. Is it posisble to output whole numbers not ones to 1 decimla place?
->>>>>>> ccbf6f6f50bc9b0b1856bdd627f49a55ec8a1e14
